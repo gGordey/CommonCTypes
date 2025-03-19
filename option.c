d52de5b423d06bd1185a2ab1460fbfd3cae8d290
gGordey <ggordeymini@gmail.com>
2025-03-19 18:42:06 +0300

    changing everything

---
+++ option.c
@@ -0,0 +1,13 @@
+#include "option.h"
+
+option _opt_new(int8_t has_halue, uint16_t byte_len) {
+    option opt;
+    opt.has_value = has_halue;
+    opt.byte_len = byte_len;
+    opt.value = malloc(byte_len);
+    return opt;
+}
+
+void _opt_set_value(option* opt, void* value) {
+    memcpy(opt->value, value, opt->byte_len);
+}
--- option.h
+++ option.h
@@ -14,16 +14,4 @@ typedef struct {
 option _opt_new(int8_t, uint16_t);
 void _opt_set_value(option*, void*);
 
-option _opt_new(int8_t has_halue, uint16_t byte_len) {
-    option opt;
-    opt.has_value = has_halue;
-    opt.byte_len = byte_len;
-    opt.value = malloc(byte_len);
-    return opt;
-}
-
-void _opt_set_value(option* opt, void* value) {
-    memcpy(opt->value, value, opt->byte_len);
-}
-
 #endif//_C_OPTION
